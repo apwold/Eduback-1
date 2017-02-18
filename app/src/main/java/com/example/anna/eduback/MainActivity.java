f42d5b5e3c34d276981b7e28abf40f9fa45ff8b9
apwold <annepwwold@gmail.com>
2017-02-18 13:27:08 +0100

    AP her

--- app/src/main/java/com/example/anna/eduback/MainActivity.java
+++ app/src/main/java/com/example/anna/eduback/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("nina");
+        System.out.println("ANNE P");
     }
 }
