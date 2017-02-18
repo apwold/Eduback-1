b5a2aad7d469c99c905deae76e82d644e6bfb20d
Anna Bellika Kjæmpenes <annabk@stud.ntnu.no>
2017-02-18 13:29:23 +0100

    anna

--- app/src/main/java/com/example/anna/eduback/MainActivity.java
+++ app/src/main/java/com/example/anna/eduback/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("nina");
         System.out.println("ANNE P");
+        System.out.println("Anna");
     }
 }
--- app/src/main/java/com/example/anna/eduback/test.java
+++
@@ -1,4 +0,0 @@
-package com.example.anna.eduback;
-public class test {
-
-}
