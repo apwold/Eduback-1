ca6fa1d5d5e7fa808990aaf82ce70ccc4a9df7cd
ninamwa <ninamwa@stud.ntnu.no>
2017-02-18 14:07:50 +0100

    MainActivityProfessor + layout + fag

--- app/src/main/java/com/example/anna/eduback/MainActivityProfessor.java
+++ app/src/main/java/com/example/anna/eduback/MainActivityProfessor.java
@@ -1,10 +1,14 @@
 package com.example.anna.eduback;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivityProfessor extends AppCompatActivity {
+public class MainActivityProfessor extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     String[] fag = new String[]{"Matematikk1", "Objektorientert Programmering", "Diskret Matematikk"};
 
@@ -14,5 +18,23 @@ public class MainActivityProfessor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_professor);
+
+       FagListView = (ListView) findViewById(R.id.FagListView);
+
+        //Lage en view for hvordan hver linje i lista skal se ut. Typ lite bilde helt til venstre, kun tekst osv.
+        //Simple list item 1 er kun tekst, nr 2 er med bilde. her kan man lage hva man vil.
+        ArrayAdapter<String> FagAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1, fag);
+
+        FagListView.setAdapter(FagAdapter);
+
+        FagListView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent moveToDetailIntent = new Intent(getBaseContext(), Fag.class);
+        moveToDetailIntent.putExtra("Fag", fag[position]);
+        startActivity(moveToDetailIntent);
+
     }
 }
