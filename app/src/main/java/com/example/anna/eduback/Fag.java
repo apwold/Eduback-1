ae61794c2150572b880a1ad17619e758941856cc
Anna Bellika Kjæmpenes <annabk@stud.ntnu.no>
2017-02-18 14:11:29 +0100

    anna commit test professor

--- app/src/main/java/com/example/anna/eduback/Fag.java
+++ app/src/main/java/com/example/anna/eduback/Fag.java
@@ -9,5 +9,7 @@ public class Fag extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fag);
+        System.out.println("Anna test");
     }
+
 }
