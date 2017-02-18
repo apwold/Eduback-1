8c53588ca0d258adc31e57ba282f67497c62fb34
Anna Bellika Kjæmpenes <annabk@stud.ntnu.no>
2017-02-18 14:38:22 +0100

    overgang mellom mainActivity og professorActivity

--- app/src/main/java/com/example/anna/eduback/MainActivity.java
+++ app/src/main/java/com/example/anna/eduback/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.anna.eduback;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("nina");
-        System.out.println("nina igjen");
-        System.out.println("anna 123");
+        testButton = (Button)findViewById(R.id.button);
+
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToTheOtherActivity = new Intent(getApplicationContext(), MainActivityProfessor.class);
+                startActivity(goToTheOtherActivity);
+            }
+        });
+
+
 
     }
 }
