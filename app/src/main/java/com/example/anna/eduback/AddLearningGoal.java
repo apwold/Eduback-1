793f3264f7a1ae1fcccfc1bd335c7950916800d7
apwold <annepwwold@gmail.com>
2017-02-20 10:53:07 +0100

    Learning Goals Activity: Lagt til expandableListView-funksjon.

--- app/src/main/java/com/example/anna/eduback/AddLearningGoal.java
+++ app/src/main/java/com/example/anna/eduback/AddLearningGoal.java
@@ -3,8 +3,17 @@ package com.example.anna.eduback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class AddLearningGoal extends AppCompatActivity {
+
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listHash;
+    EditText addLearningGoal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +25,24 @@ public class AddLearningGoal extends AppCompatActivity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
         getWindow().setLayout((int) (width*.8), (int) (height*.5));
+
+        addLearningGoal = (EditText) findViewById(R.id.addLGEditText);
+        String newLG = addLearningGoal.toString();
+        addLG(newLG);
+
+
+    }
+
+    private void addLG(String lg) {
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        List<String> nyttLG = new ArrayList<>();
+        nyttLG.add("Ingen forståelse");
+        nyttLG.add("Delvis forståelse");
+        nyttLG.add("Full forståelse");
+
+        listHash.put(listDataHeader.get(listDataHeader.size()), nyttLG);
+
     }
 }
--- app/src/main/java/com/example/anna/eduback/LearningGoals.java
+++ app/src/main/java/com/example/anna/eduback/LearningGoals.java
@@ -3,23 +3,47 @@ package com.example.anna.eduback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class LearningGoals extends AppCompatActivity implements View.OnClickListener{
 
+    private ExpandableListView listView;
+    private ExpandableListAdapter listAdapter;
+    private List<String> listDataHeader = new ArrayList<>();
+    private HashMap<String, List<String>> listHash = new HashMap<>();
     Button addLG;
+    String newLG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning_goals);
-        System.out.println("Denne siden er for å legge inn og vise læringsmål");
+
+        listView = (ExpandableListView) findViewById(R.id.expLV);
+        addLGoal(newLG);
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listHash);
+        listView.setAdapter(listAdapter);
 
         addLG = (Button) findViewById(R.id.baddLG);
         addLG.setOnClickListener(this);
 
+
     }
 
+    private void addLGoal(String newLG) {
+        listDataHeader.add(newLG);
+        List<String> nyttlaringsmal = new ArrayList<>();
+        nyttlaringsmal.add("Ingen forståelse");
+        nyttlaringsmal.add("Delvis forståelse");
+        nyttlaringsmal.add("Full forståelse");
+        listHash.put(listDataHeader.get(listDataHeader.size()), nyttlaringsmal);
+    }
+
+    
     @Override
     public void onClick(View v) {
 
