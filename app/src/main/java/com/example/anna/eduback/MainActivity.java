cd8ab281607d67c6c1b20f6b72d4d59d4ea97523
ninamwa <ninamwa@stud.ntnu.no>
2017-02-20 13:07:09 +0100

    Lagt til læringsmål knapp på hovedsiden

--- app/src/main/java/com/example/anna/eduback/MainActivity.java
+++ app/src/main/java/com/example/anna/eduback/MainActivity.java
@@ -8,12 +8,22 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button testButton;
+    Button LGButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         testButton = (Button)findViewById(R.id.button);
+        LGButton = (Button) findViewById(R.id.LGButton);
+
+        LGButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToLG = new Intent(getApplicationContext(),LearningGoals.class);
+                startActivity(goToLG);
+            }
+        });
 
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
